7d952b2eb9d0798dfca7ef03e3d198d3fe425b93
OukoFranchez <oukofranchez@gmail.com>
2023-03-05 14:01:39 -0800

    Added comments.c for learning comments in C

---
+++ cShenanigans/commentsInC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+//creating a variable
+	int integer = 34;
+
+//printing the created variable in a new lin  e
+
+	printf("integer = %d\n", integer);
+	
+		return 0;
+}
+//we can also use comments in debugging our code by adding the 2 forward slashes infront of the code that we want to debug
+//we can also use /* at the beginning and end

8b46262304975bec95b137267b1f219ec6dfe38f
OukoFranchez <oukofranchez@gmail.com>
2023-03-05 15:59:25 -0800

    Added a file BooleanandComparisonOperatos for purposes learning Boolean and Comparison operators

---
+++ cShenanigans/booleanAndComparisonOperators.c
@@ -0,0 +1,8 @@
+//Boolean is a data type that can store only two values; true and false
+//However, to create boolean we must #include <stdbool.h> file
+//in C, boolean values are represented as int data type and we therefore print with %d and true is represented by 1 and false represented by 0
+#include <stdio.h>
+#include <stdbool.h>
+
+
+

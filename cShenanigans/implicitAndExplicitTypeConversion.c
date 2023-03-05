72cbc8d1dde7366e9071b1e4fc94cd684e6dddef
OukoFranchez <oukofranchez@gmail.com>
2023-03-05 15:28:58 -0800

    added a file implicitandExplicitConversion for purposes learning implicit and explicit conversion in C

---
+++ cShenanigans/implicitAndExplicitTypeConversion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+//	 char a = '5';
+
+//	 int b = 9;
+
+//	 int result = a + b;
+
+//	 printf("Result = %d\n", result);
+
+//	int result = '8' + 12;
+
+//	printf("Result = %d\n", result);
+
+//explicit conversion
+
+	int a = 9;
+
+	int b = 2;
+
+	double result =  a / b;
+
+	printf("Converted Result = %lf\n", result);
+
+	 return 0;
+}
